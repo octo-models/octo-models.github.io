6525669e39cdeb3f168ef3e6a51957635924731a
Kevin Black <kevin3.black@gmail.com>
2023-12-14 15:24:38 -0600

    Add more stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,6 +69,15 @@
 				Code
 			</a>
 		</div>
+		<div class="flex flex-col justify-center hover:bg-sky-200 rounded-md">
+			<a
+				href="https://huggingface.co/rail-berkeley"
+				class="hover:underline text-black p-4 flex flex-col items-center"
+			>
+				<img src="{base}/weights.svg" alt="weights" class="w-16" />
+				Weights
+			</a>
+		</div>
 	</div>
 </div>
 
@@ -78,16 +87,147 @@
 </div>
 
 <!-- content column again -->
-<div class="max-w-6xl w-full px-2 pt-4 flex flex-col items-center mx-auto">
+<div class="max-w-6xl w-full px-2 pt-4 mx-auto">
 	<p class="mt-8 text-l">
 		We introduce <Octo />, our ongoing effort for building open-source, widely applicable generalist
 		policies for robotic manipulation. The Octo model is a transformer-based diffusion policy,
 		pretrained on 800k robot episodes from the
 		<a href="https://robotics-transformer-x.github.io/">Open X-Embodiment dataset</a>. It supports
 		flexible task and observation definitions and can be quickly finetuned to new observation and
-		action spaces.
+		action spaces. We are introducing two initial versions of Octo, Octo-Small (27M parameters) and
+		Octo-Base (93M parameters).
 	</p>
-	<img src="{base}/teaser.png" alt="teaser" class="w-full" />
+	<img src="{base}/teaser.jpg" alt="teaser" class="w-full mt-16 px-16" />
+
+	<h2 class="text-4xl mt-16">The Model</h2>
+	<p class="mt-8">
+		The design of the Octo model emphasizes flexibility and scale: the model is designed to support
+		a variety of commonly used robots, sensor configurations, and actions, while providing a generic
+		and scalable recipe that can be trained on large amounts of data. Octo supports both natural
+		language instructions and goal images, observation histories, and multi-modal action
+		distributions via diffusion decoding. Furthermore, we designed Octo specifically to support
+		efficient finetuning to new robot setups, including robots with different actions and different
+		combinations of cameras and proprioceptive information. This design was selected specifically to
+		make Octo a flexible and broadly applicable generalist robotic policy that can be utilized for a
+		variety of downstream robotics applications and research projects.
+
+		<img src="{base}/architecture.jpg" alt="model" class="w-full mt-16 px-16" />
+	</p>
+
+	<h2 class="text-4xl mt-16">The Data</h2>
+
+	<p class="mt-8">
+		We train Octo on a mixture of 25 datasets from the Open X-Embodiment Dataset, a diverse
+		collection of robot learning datasets. Our training mixture includes data from a variety of
+		robot embodiments, scenes, and tasks. These datasets are heterogeneous not just in terms of the
+		robot type, but also in the sensors (e.g., including or not including wrist cameras) and labels
+		(e.g., including or not including language instructions).
+		<img src="{base}/sampling_weights.jpg" alt="model" class="w-3/4 mt-16 mx-auto" />
+	</p>
+
+	<h2 class="text-4xl mt-16">The Results</h2>
+	<p class="mt-8">
+		We evaluate Octo on 6 real robot setups across 3 institutions. Our evaluations capture diverse
+		object interactions (e.g., "WidowX BridgeV2"), long task horizons (e.g., "Stanford Coffee") and
+		precise manipulation (e.g., "Berkeley Peg Insert"). We evaluate Octo's capabilities to control
+		robots in environments from the pretraining data out-of-the-box and to efficiently finetune to
+		new tasks and environments with small target domain datasets. We also test finetuning with new
+		observations (force-torque inputs for "Berkeley Peg Insert") and action spaces (joint position
+		control in "Berkeley Pick-up").
+		<img src="{base}/exp_setups.jpg" alt="model" class="w-full mt-16 px-8 mx-auto" />
+	</p>
+
+	<div class="flex flex-nowrap justify-between mt-16">
+		
+		<table class="w-1/4 mx-8 border-collapse text-center text-xs md:text-base">
+			<thead>
+				<tr>
+					<th colspan="3" class="text-lg">Zero-shot</th>
+				</tr>
+				<tr class="border-b border-t-2 border-black">
+					<th />
+					<th>WindowX Sink</th>
+					<th>UR5</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>RT-1-X</td>
+					<td>0.00</td>
+					<td>0.00</td>
+				</tr>
+				<tr>
+					<td>RT-2-X</td>
+					<td>0.00</td>
+					<td>0.00</td>
+				</tr>
+				<tr class="border-b-2 border-black">
+					<td><Octo/></td>
+					<td>0.00</td>
+					<td>0.00</td>
+				</tr>
+			</tbody>
+		</table>
+		<table class="w-3/4 mx-8 border-collapse text-center text-xs md:text-base">
+			<thead>
+				<tr>
+					<th colspan="6" class="text-lg">Finetuning</th>
+				</tr>
+				<tr class="border-b border-t-2 border-black">
+					<th />
+					<th>CMU Baking</th>
+					<th>Stanford Coffee</th>
+					<th>Berkeley Peg Insert<sup>*</sup></th>
+					<th>Berkeley Pick-up<sup>†</sup></th>
+					<th>Average</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>From Scratch</td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+				</tr>
+				<tr>
+					<td>VC-1</td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+				</tr>
+				<tr class="border-b-2 border-black">
+					<td><Octo/></td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+					<td>0.00</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="mt-2 flex justify-center text-xs md:text-base">
+        <div class="px-4"><sup>*</sup>New observation input (force-torque proprioception)</div>
+        <div class="px-4"><sup>†</sup>New action space (joint position control)</div>
+    </div>
 </div>
 
 <div class="pb-[1000px]" />
+
+<style>
+	th {
+		font-weight: normal;
+	}
+
+	tr > td:first-child {
+		text-align: left;
+	}
+
+	td  {
+		line-height: 2rem;
+	}
+</style>
